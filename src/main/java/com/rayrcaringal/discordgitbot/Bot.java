da141635b25f88935ae3f81db844de406cbd97df
Raymund Caringal <Rayrcaringal@gmail.com>
2019-10-26 00:45:31 -0400

    Fix

--- src/main/java/com/rayrcaringal/discordgitbot/Bot.java
+++ src/main/java/com/rayrcaringal/discordgitbot/Bot.java
@@ -4,7 +4,7 @@ import net.dv8tion.jda.core.JDABuilder;
 public class Bot {
 
     public static void main(String[] args) throws Exception {
-        JDA jda = new JDABuilder("NjM3NTA3NDU3MjcwMTUzMjMw.XbPLQg.KBKsnpQys-vrrxRRXN7nj4p0SpE").build();
+        JDA jda = new JDABuilder("NjM3NTA3NDU3MjcwMTUzMjMw.XbPPHg.TE-vVJNZ9XALx6CkfZXWDyFVR8w").build();
     }
 
 }
