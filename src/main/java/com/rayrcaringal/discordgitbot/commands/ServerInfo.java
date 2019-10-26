4818fdb362524431c5ecbb0ae3efc0f01e3bb083
Raymund Caringal <Rayrcaringal@gmail.com>
2019-10-26 18:30:49 -0400

    Added JDA Utilities

---
+++ src/main/java/com/rayrcaringal/discordgitbot/commands/ServerInfo.java
@@ -0,0 +1,30 @@
+package com.rayrcaringal.discordgitbot.commands;
+
+import java.awt.Color;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+
+public class ServerInfo extends Command{
+
+    public ServerInfo(){
+        this.name = "serverinfo";
+        this.aliases = new String[]{"server"};
+        this.help = "Infromation about server";
+    }
+    
+    @Override
+    protected void execute(CommandEvent event){
+        EmbedBuilder eb = new EmbedBuilder();
+        eb.setColor(Color.GREEN);
+        eb.setAuthor(event.getGuild().getName());
+        eb.setThumbnail("https://i.imgur.com/OO5WY0n.png");
+        event.getChannel().sendMessage(eb.build()).queue();;
+        
+    }
+
+
+
+}
