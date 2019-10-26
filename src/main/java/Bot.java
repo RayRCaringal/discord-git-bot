94e15089ddaa443e56721a9e5fc8a60d7547ba57
Raymund Caringal <Rayrcaringal@gmail.com>
2019-10-26 00:41:30 -0400

    Updated Dependencies

---
+++ src/main/java/Bot.java
@@ -0,0 +1,10 @@
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+
+public class Bot {
+
+    public static void main(String[] args) throws Exception {
+        JDA jda = new JDABuilder("NjM3NTA3NDU3MjcwMTUzMjMw.XbPLQg.KBKsnpQys-vrrxRRXN7nj4p0SpE").build();
+    }
+
+}
