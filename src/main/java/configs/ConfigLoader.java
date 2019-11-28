42a687e29f8bd8ffba3020fd1c4f4f53d4fe4ee3
RayRCaringal <rayrcaringal@gmail.com>
2019-11-27 23:35:50 -0500

    Added UserRepo

---
+++ src/main/java/configs/ConfigLoader.java
@@ -0,0 +1,13 @@
+package configs;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class ConfigLoader {
+    String load(File file) throws IOException{
+        return new String(Files.readAllBytes(file.toPath()))    ;
+    }
+
+}
+
