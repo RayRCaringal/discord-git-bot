ebced7ada4647422cae0d12db7a8f80589f68b1d
RayRCaringal <rayrcaringal@gmail.com>
2019-11-29 13:28:10 -0500

    Updated for multiple names

--- src/main/java/com/rayrcaringal/discordgitbot/commands/UserRepo.java
+++ src/main/java/com/rayrcaringal/discordgitbot/commands/UserRepo.java
@@ -38,22 +38,22 @@ public class UserRepo extends Command {
         }
     }
 
-    void send(int i, CommandEvent event){
-        int newLimit = i+limit-1;
+    void send(int i, CommandEvent event) {
+        int newLimit = i + limit - 1;
         list = "";
-        System.out.println("i is " + i + "Repo is " +repos.size());
-        for(; i < repos.size(); i++){
-            System.out.println("here");
-            String temp = String.valueOf(i + 1);
-            list = list + temp + ". " + repos.get(i) + "\n";
+        System.out.println("i is " + i + "Repo is " + repos.size());
+        for (; i < repos.size(); i++) {
+           // System.out.println("here");
+            String temp = "**" + String.valueOf(i + 1);
+            list = list + temp + ".** " + repos.get(i) + "\n";
             eb.setDescription(list);
-            if (i == newLimit-1) {
-                int counter = limit + i+1;
+            if (i == newLimit - 1) {
+                int counter = limit + i + 1;
                 event.getChannel().sendMessage(eb.build()).complete().addReaction("U+27A1").queue();
                 waiter.waitForEvent(GuildMessageReactionAddEvent.class,
                         e -> e.getChannel().equals(event.getChannel())
                                 && e.getUser().equals(event.getAuthor())
-                        , e-> send( counter, event),
+                        , e -> send(counter, event),
                         30, TimeUnit.SECONDS, () -> event.reply("Please input your selected path"));
                 return;
             }
@@ -66,7 +66,7 @@ public class UserRepo extends Command {
     protected void execute(CommandEvent event) {
         if ((event.getArgs().isEmpty()) || (event.getArgs().contains(" ") && !(event.getArgs().contains("/")))) {
             event.reply(event.getAuthor().getAsMention() + " provide a path to a repository: $userRepo [Username]/[Repository Name]");
-        }else {
+        } else {
             GitHub github = null;
             try {
                 github = GitHub.connectAnonymously();
@@ -80,40 +80,52 @@ public class UserRepo extends Command {
             } else {// Provide List of Repos based on user name
                 list = "";
                 String name = event.getArgs();
-                System.out.println("Name is " +name + " Count is "+github.searchUsers().q(name).list().getTotalCount() );
+                System.out.println("Name is " + name + " Count is " + github.searchUsers().q(name).list().getTotalCount());
                 repos.clear();
+
+                eb.setColor(Color.GREEN);
+                if (github.searchUsers().q(name).list().getTotalCount() > 1) {
+                    eb.setTitle("There are multiple users with this name");
+                    for (GHUser user : github.searchUsers().q(name).list()) {
+                        list = list + user.getLogin() + ": " + user.getHtmlUrl() + "\n";
+                        eb.setDescription(list);
+                    }
+                    event.reply(eb.build());
+                    eb.setFooter("If this is the incorrect user please input a different user according to the list above");
+                }
+
+                list = "";
+                //Gets the first user
                 if (github.searchUsers().q(name).list().getTotalCount() > 0) {
-                    GHUser user = Iterables.get(github.searchUsers().q(name).list(), 0 );
+                    GHUser user = Iterables.get(github.searchUsers().q(name).list(), 0);
                     for (GHRepository repo : user.listRepositories()) {
                         if (!(repo.isPrivate())) {
                             repos.add(repo.getFullName());
                         }
                     }
-
-                    eb.setColor(Color.GREEN);
                     eb.setTitle(user.getHtmlUrl().toString());
                     eb.setThumbnail(user.getAvatarUrl());
                     System.out.println("Here");
                     for (int i = 0; i < repos.size(); i++) {
-                        System.out.println("i is " + i + " Repo is " +repos.size());
-                        String temp = String.valueOf(i + 1);
-                        list = list + temp + ". " + repos.get(i) + "\n";
+                        System.out.println("i is " + i + " Repo is " + repos.size());
+                        String temp = "**" + String.valueOf(i + 1);
+                        list = list + temp + ".** " + repos.get(i) + "\n";
                         eb.setDescription(list);
-                        if (i == limit-1){
+                        if (i == limit - 1) {
                             event.getChannel().sendMessage(eb.build()).complete().addReaction("U+27A1").queue();
                             waiter.waitForEvent(GuildMessageReactionAddEvent.class,
                                     e -> e.getChannel().equals(event.getChannel())
                                             && e.getUser().equals(event.getAuthor())
                                     , e -> {
-                                  int counter = limit;
-                                  send( counter, event);
+                                        int counter = limit;
+                                        send(counter, event);
                                     },
-                            30, TimeUnit.SECONDS, () -> event.reply("Please input your selected path"));
+                                    30, TimeUnit.SECONDS, () -> event.reply("Please input your selected path"));
                             break;
                         }
 
                     }
-                    if(repos.size() < limit){
+                    if (repos.size() < limit) {
                         eb.setDescription(list);
                         event.reply(eb.build());
                     }
