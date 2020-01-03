970d1551273dd3fb550b8a58460de28ef7e71893
Raymund Caringal <Rayrcaringal@gmail.com>
2020-01-02 23:37:09 -0500

    Fixed Formatting

--- src/main/java/com/rayrcaringal/discordgitbot/commands/DelRepo.java
+++ src/main/java/com/rayrcaringal/discordgitbot/commands/DelRepo.java
@@ -2,7 +2,6 @@ package com.rayrcaringal.discordgitbot.commands;
 
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
-import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import configs.Config;
 import connection.Repo;
 import connection.impl.RepoImpl;
